5de7eb85fa37655c86cbdede978b50ad75b1c8e0
unknown <sarasaravevic@gmail.com>
2022-08-12 12:26:12 +0200

    first steps

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import ExpnesItem from "./components/ExpenseItem";
 
 function App() {
-  return <div className="App">never give up</div>;
+  return (
+    <div className="App">
+      <ExpnesItem />
+    </div>
+  );
 }
 
 export default App;
