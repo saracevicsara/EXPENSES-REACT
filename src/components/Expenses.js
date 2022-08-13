44371362c805620da2c0805860d42b2a31bf33c6
unknown <sarasaravevic@gmail.com>
2022-08-13 17:02:06 +0200

    error

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,30 +1,9 @@
 import ExpensItem from "./ExpenseItem";
 import "./Expenses.css";
+import Card from "./Card";
 function Expenses(props) {
-  //   const expenses = [
-  //     {
-  //       title: "Car Insurance",
-  //       amount: 294.67,
-  //       date: new Date(2021, 2, 28),
-  //     },
-  //     {
-  //       title: "toilet paper",
-  //       amount: 94.67,
-  //       date: new Date(2021, 2, 25),
-  //     },
-  //     {
-  //       title: "New TV",
-  //       amount: 350.87,
-  //       date: new Date(2021, 7, 28),
-  //     },
-  //     {
-  //       title: "New Desk (Wooden)",
-  //       amount: 120.67,
-  //       date: new Date(2021, 3, 15),
-  //     },
-  //   ];
   return (
-    <div className="Expenses">
+    <Card className="Expenses">
       <ExpensItem
         title={props.items[0].title}
         amount={props.items[0].amount}
@@ -45,7 +24,7 @@ function Expenses(props) {
         amount={props.items[3].amount}
         date={props.items[3].date}
       />
-    </div>
+    </Card>
   );
 }
 export default Expenses;
