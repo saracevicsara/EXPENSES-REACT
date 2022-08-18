a482b495936b91d7c290ddf210d2e99abf339ced
unknown <sarasaravevic@gmail.com>
2022-08-18 20:18:44 +0200

    state

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,8 +1,15 @@
 import ExpenseDate from "./ExpenseDate.js";
 import Card from "../UI/Card";
 import "./ExpenseItem.css";
+import { useState } from "react";
 //
 function ExpensItem(props) {
+  const [title, setTitle] = useState(props.title);
+  console.log("expenseoItem are eveluated by react");
+  const clickhandler = () => {
+    setTitle("updated title");
+    console.log(title);
+  };
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
@@ -10,6 +17,7 @@ function ExpensItem(props) {
         <h2>{props.title}</h2>
         <div className="expense-item__price">$ {props.amount}</div>
       </div>
+      <button onClick={clickhandler}>change title</button>
     </Card>
   );
 }
