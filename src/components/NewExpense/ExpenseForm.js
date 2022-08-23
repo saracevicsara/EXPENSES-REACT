e3224d144962a033584d56245ddaa7302953dd49
unknown <sarasaravevic@gmail.com>
2022-08-23 19:41:15 +0200

    tuga

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -16,6 +16,12 @@ const ExpenseForm = () => {
   const submitHandler = (event) => {
     event.preventDefault();
   };
+  const expenseData = {
+    title: enteredTitle,
+    amount: enteredAmount,
+    date: new Date(enteredDate),
+  };
+  console.log(expenseData);
   return (
     <form onSubmit={submitHandler}>
       <div className="new-expense__controls">
