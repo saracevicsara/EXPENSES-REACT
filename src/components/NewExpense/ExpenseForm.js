382cbcf57ae51d2c9249bb68c2fb8b259b26b314
unknown <sarasaravevic@gmail.com>
2022-08-25 20:22:30 +0200

    handling form

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -14,14 +14,14 @@ const ExpenseForm = () => {
     setEnteredDate(event.target.value);
   };
   const submitHandler = (event) => {
-    event.preventDefault();
+    const expenseData = {
+      title: enteredTitle,
+      amount: enteredAmount,
+      date: new Date(enteredDate),
+    };
+    console.log(expenseData);
   };
-  const expenseData = {
-    title: enteredTitle,
-    amount: enteredAmount,
-    date: new Date(enteredDate),
-  };
-  console.log(expenseData);
+
   return (
     <form onSubmit={submitHandler}>
       <div className="new-expense__controls">
